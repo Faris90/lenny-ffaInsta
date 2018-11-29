bf3715d40464e383d93095abcc4f7ed4f22aad58
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2018-11-29 08:51:22 -0600

    Update index.js

--- src/entity/index.js
+++ src/entity/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   Cell: require('./Cell'),
   PlayerCell: require('./PlayerCell'),
-  Food: require('./NewFood'),
+  Food: require('./Food'),
   Virus: require('./Virus'),
   EjectedMass: require('./EjectedMass'),
   MovingVirus: require('./MovingVirus.js'),
